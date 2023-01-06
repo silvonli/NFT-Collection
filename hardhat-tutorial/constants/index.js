f3048957f7e01874bda33cd034415be67ee615a5
silvon <silvonli@gmail.com>
2023-01-06 18:04:17 +0800

    修改配置

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x98f5c62F7b5Bc735a64942dBec9456954c7a368A";
+const WHITELIST_CONTRACT_ADDRESS = "0x3d9d5D83441cE52BB76071D92895977D157b7765";
 // URL for the Crypto Devs NFT Metadata
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
